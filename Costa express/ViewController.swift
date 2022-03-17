bb78c94c40d382ae2a6c791b3a27d10e3d7776c0
groot737 <giorgiquchuloria7@gmail.com>
2022-03-18 02:50:41 +0400

    Initial Commit

---
+++ Costa express/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Costa express
+//
+//  Created by giorgi on 3/18/22.
+//  Copyright © 2022 Gai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Costa expressTests/Costa_expressTests.swift
@@ -0,0 +1,34 @@
+//
+//  Costa_expressTests.swift
+//  Costa expressTests
+//
+//  Created by giorgi on 3/18/22.
+//  Copyright © 2022 Gai. All rights reserved.
+//
+
+import XCTest
+@testable import Costa_express
+
+class Costa_expressTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
